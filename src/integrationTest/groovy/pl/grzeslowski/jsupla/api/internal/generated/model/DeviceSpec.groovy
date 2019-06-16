02c57dcf63f2462c1febe3e7bc934a2fb1d89a41
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2019-06-16 20:45:54 +0200

    #32 Better test name

--- src/integrationTest/groovy/pl/grzeslowski/jsupla/api/internal/generated/model/DeviceSpec.groovy
+++ src/integrationTest/groovy/pl/grzeslowski/jsupla/api/internal/generated/model/DeviceSpec.groovy
@@ -4,12 +4,12 @@ import io.swagger.client.model.Device
 import spock.lang.Specification
 
 class DeviceSpec extends Specification {
-	def "should have String as lastIpv4"() {
+	def "should have Long as lastIpv4"() {
 		expect:
 		new Device(lastIpv4: 1).lastIpv4 instanceof Long
 	}
 
-	def "should have String as regIpv4"() {
+	def "should have Long as regIpv4"() {
 		expect:
 		new Device(regIpv4: 1).regIpv4 instanceof Long
 	}
