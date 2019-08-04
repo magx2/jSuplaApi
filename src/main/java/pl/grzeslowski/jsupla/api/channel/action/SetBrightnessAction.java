514339596e926d80775b957bcd9fc3f7dff4ef87
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2019-08-04 21:36:22 +0200

    Inform what was illegal argument

--- src/main/java/pl/grzeslowski/jsupla/api/channel/action/SetBrightnessAction.java
+++ src/main/java/pl/grzeslowski/jsupla/api/channel/action/SetBrightnessAction.java
@@ -10,10 +10,10 @@ public class SetBrightnessAction implements Action {
 
     public SetBrightnessAction(final int brightness) {
         if (brightness > 100) {
-            throw new IllegalArgumentException("Brightness cannot be grater than 100!");
+            throw new IllegalArgumentException("Brightness cannot be grater than 100! Was `" + brightness + "`.");
         }
         if (brightness < 0) {
-            throw new IllegalArgumentException("Brightness cannot be smaller than 0!");
+            throw new IllegalArgumentException("Brightness cannot be smaller than 0! Was `" + brightness + "`.");
         }
         this.brightness = brightness;
     }
