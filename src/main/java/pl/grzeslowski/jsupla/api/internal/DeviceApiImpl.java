d4a037bbcd161802e90c2fbe82d9a0d3a808c04c
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2020-05-30 10:19:49 +0200

    CLOSE #76 Optimize Device loading

--- src/main/java/pl/grzeslowski/jsupla/api/internal/DeviceApiImpl.java
+++ src/main/java/pl/grzeslowski/jsupla/api/internal/DeviceApiImpl.java
@@ -4,6 +4,7 @@ import io.swagger.client.ApiClient;
 import io.swagger.client.ApiException;
 import io.swagger.client.api.IoDevicesApi;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import pl.grzeslowski.jsupla.api.DeviceApi;
 import pl.grzeslowski.jsupla.api.channel.Channel;
 import pl.grzeslowski.jsupla.api.device.Device;
@@ -11,9 +12,11 @@ import pl.grzeslowski.jsupla.api.device.Device;
 import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toMap;
 import static pl.grzeslowski.jsupla.api.internal.ToStringHelper.niceToString;
 
 @Slf4j
@@ -30,7 +33,8 @@ final class DeviceApiImpl implements DeviceApi {
     @Override
     public Device findDevice(int id) {
         try {
-            return mapToDeviceWithChannels(ioDevicesApi.getIoDevice(id, DEFAULT_INCLUDE));
+            val channels = findChannels();
+            return mapToDeviceWithChannels(ioDevicesApi.getIoDevice(id, DEFAULT_INCLUDE), channels);
         } catch (ApiException e) {
             throw new pl.grzeslowski.jsupla.api.ApiException("/findDevice/" + id, e);
         }
@@ -39,20 +43,29 @@ final class DeviceApiImpl implements DeviceApi {
     @Override
     public SortedSet<Device> findDevices() {
         try {
+            val channels = findChannels();
             return ioDevicesApi.getIoDevices(DEFAULT_INCLUDE)
                            .stream()
-                           .map(this::mapToDeviceWithChannels)
+                           .map((io.swagger.client.model.Device device) -> mapToDeviceWithChannels(device, channels))
                            .collect(Collectors.toCollection(TreeSet::new));
         } catch (ApiException e) {
             throw new pl.grzeslowski.jsupla.api.ApiException("/findDevices", e);
         }
     }
 
-    private Device mapToDeviceWithChannels(final io.swagger.client.model.Device device) {
+    private SortedSet<Channel> findChannels() {
+        log.debug("Loading all channels to build device");
+        return channelApi.findChannels();
+    }
+
+    private Device mapToDeviceWithChannels(final io.swagger.client.model.Device device,
+                                           final SortedSet<Channel> cloudChannels) {
         log.trace("Got device {}", niceToString(device));
+        val channelsMap = cloudChannels.stream().collect(toMap(Channel::getId, Function.identity()));
         final SortedSet<Channel> channels = device.getChannelsIds()
                                                     .stream()
-                                                    .map(channelApi::findChannel)
+                                                    .filter(channelsMap::containsKey)
+                                                    .map(channelsMap::get)
                                                     .collect(Collectors.toCollection(TreeSet::new));
         return new DeviceImpl(device, channels);
     }
