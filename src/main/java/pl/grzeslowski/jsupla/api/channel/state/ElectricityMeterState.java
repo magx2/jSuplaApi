22914ac841747f5d29e083fba9f0d22ea35dbafa
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2020-05-30 18:34:00 +0200

    CLOSE #80 Add phases summary to electric meter

--- src/main/java/pl/grzeslowski/jsupla/api/channel/state/ElectricityMeterState.java
+++ src/main/java/pl/grzeslowski/jsupla/api/channel/state/ElectricityMeterState.java
@@ -10,33 +10,115 @@ public interface ElectricityMeterState extends State {
 
     String getCurrency();
 
+    PhasesSummary getPhasesSummary();
+
     SortedSet<Phase> getPhases();
+
+    interface PhasesSummary {
+        int getNumberOfPhases();
+
+        /**
+         * Frequency in <code>Hz</code>.
+         */
+        BigDecimal getFrequency();
+
+        /**
+         * Active power in <code>W</code>
+         */
+        BigDecimal getPowerActive();
+
+        /**
+         * Reactive power in <code>var</code>
+         */
+        BigDecimal getPowerReactive();
+
+        /**
+         * Apparent power in <code>VA</code>
+         */
+        BigDecimal getPowerApparent();
+
+        /**
+         * Total forward active energy in <code>kWh</code>
+         */
+        BigDecimal getTotalForwardActiveEnergy();
+
+        /**
+         * Total reverse active energy in <code>kWh</code>
+         */
+        BigDecimal getTotalReverseActiveEnergy();
+
+        /**
+         * Total forward reactive energy in <code>kvarh</code>
+         */
+        BigDecimal getTotalForwardReactiveEnergy();
+
+        /**
+         * Total reverse reactive energy in <code>kvarh</code>
+         */
+        BigDecimal getTotalReverseReactiveEnergy();
+    }
 
     interface Phase extends Comparable<Phase> {
         int getNumber();
 
+        /**
+         * Frequency in <code>Hz</code>.
+         */
         BigDecimal getFrequency();
 
+        /**
+         * Voltage in <code>V</code>
+         */
         BigDecimal getVoltage();
 
+        /**
+         * Current in <code>A</code>
+         */
         BigDecimal getCurrent();
 
+        /**
+         * Active power in <code>W</code>
+         */
         BigDecimal getPowerActive();
 
+        /**
+         * Reactive power in <code>var</code>
+         */
         BigDecimal getPowerReactive();
 
+        /**
+         * Apparent power in <code>VA</code>
+         */
         BigDecimal getPowerApparent();
 
+        /**
+         * Power factor. No unit
+         */
         BigDecimal getPowerFactor();
 
+        /**
+         * Phase angle in <code>°</code>
+         */
         BigDecimal getPhaseAngle();
 
+        /**
+         * Total forward active energy in <code>kWh</code>
+         */
         BigDecimal getTotalForwardActiveEnergy();
 
+        /**
+         * Total reverse active energy in <code>kWh</code>
+         */
         BigDecimal getTotalReverseActiveEnergy();
 
+        /**
+         * Total forward reactive energy in <code>kvarh</code>
+         */
         BigDecimal getTotalForwardReactiveEnergy();
 
+        /**
+         * Total reverse reactive energy in <code>kvarh</code>
+         */
         BigDecimal getTotalReverseReactiveEnergy();
 
         @Override
